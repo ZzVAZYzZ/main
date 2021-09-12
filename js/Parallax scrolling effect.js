f06c86b2db497f4fe2f2684056273d6dab0e35ea
ZzVAZYzZ <haiminh454@yahoo.com>
2021-09-13 03:01:36 +0900

    hello

---
+++ js/Parallax scrolling effect.js
@@ -0,0 +1,15 @@
+let start=document.getElementById("start");
+let middle=document.getElementById("middle");
+let end=document.getElementById("end");
+let text=document.getElementById("text");
+let sun=document.getElementById("sun");
+
+window.addEventListener("scroll",function(){
+    var value = window.scrollY;
+
+    end.style.top = value *1 + 'px';
+    middle.style.top = value *0.5 + 'px';
+    text.style.top = value *2.8 + 'px';
+    sun.style.left = value *3 +'px';
+    sun.style.top = value *1.5 + 'px';
+})
