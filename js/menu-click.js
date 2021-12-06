2a54c89bcf8561a7ae575dc0e269bf641165f460
ZzVAZYzZ <haiminh454@yahoo.com>
2021-12-06 21:38:52 +0900

    changed

--- js/menu-click.js
+++ js/menu-click.js
@@ -1,4 +1,15 @@
+let nav=document.getElementById("nav-toggle");
+let open_icon=document.getElementById("open_icon");
+let close_icon=document.getElementById("close_icon");
 
 function sclick(){
-    document.getElementById("menu-toggle");
+    nav.style.display= "block";
+    open_icon.style.display="none";
+    close_icon.style.display="block";
+    
+}
+function xclick(){
+    nav.style.display= "none";
+    open_icon.style.display="block";
+    close_icon.style.display="none";
 }
